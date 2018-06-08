6bc9bd4ed63c74d5b4a357865bf1ea5e0b8160f4
intro-swf <33427068+intro-swf@users.noreply.github.com>
2018-06-08 11:24:29 +0100

    Update tokentown.js

--- tokentown.js
+++ tokentown.js
@@ -2,9 +2,29 @@ define([], function() {
 
   function Token() {
   }
+  
+  function CallToken() {
+  }
+  CallToken.prototype = Object.create(Token.prototype);
+  
+  function MethodCallToken() {
+  }
+  MethodCallToken.prototype = Object.create(CallToken.prototype);
+
+  function FunctionCallToken() {
+  }
+  FunctionCallToken.prototype = Object.create(CallToken.prototype);
+  
+  function LookupToken() {
+  }
+  LookupToken.prototype = Object.create(Token.prototype);
 
   return {
     Token: Token,
+    CallToken: CallToken,
+    MethodCallToken: MethodCallToken,
+    FunctionCallToken: FunctionCallToken,
+    LookupToken: LookupToken,
   };
 
 });
