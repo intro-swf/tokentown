887f4bedf939ca72bef09f96fcb6877c24840599
intro-swf <33427068+intro-swf@users.noreply.github.com>
2018-06-29 12:58:43 +0100

    Update struct.js

--- struct.js
+++ struct.js
@@ -9,14 +9,171 @@ define(function() {
   }
   StructFieldDef.prototype = {
     name: undefined,
+    getDataView: function(buffer, byteOffset) {
+      const byteLength = this.fixedByteLength;
+      if (!isNaN(byteLength)) {
+        return new DataView(buffer, byteOffset, byteLength);
+      }
+      throw new Error('getDataView not implemented');
+    },
+    readValue: function(dataView, byteOffset) {
+      throw new Error('readValue not implemented');
+    },
+    writeValue: function(dataView, byteOffset) {
+      throw new Error('writeValue not implemented');
+    },
+    minByteLength: NaN,
+    maxByteLength: NaN,
+    get fixedByteLength() {
+      if (isNaN(this.minByteLength) || !this.isFinite(this.minByteLength) || this.minByteLength !== this.maxByteLength) {
+        return NaN;
+      }
+      return this.minByteLength;
+    },
+    set fixedByteLength(n) {
+      this.minByteLength = this.maxByteLength = n;
+    },
+    paddingAlignment: 1,
+    addPropertyDescriptors: function(obj, struct, name) {
+      if (!(struct instanceof StructDef)) {
+        throw new Error('generating descriptor requires struct def');
+      }
+      if (typeof name !== 'string') {
+        throw new Error('generating descriptor requires field name');
+      }
+      const i = struct.fieldOrder.indexOf(name);
+      if (i === -1) {
+        throw new Error('field not found: ' + name);
+      }
+      let byteOffset = -1;
+      let prevName = null;
+      let firstAnon_i = i;
+      while (firstAnon_i > 0 && typeof struct.fieldOrder[firstAnon_i] === 'object') {
+        firstAnon_i--;
+      }
+      if (firstAnon_i < i) {
+        if (firstAnon_i === 0) {
+          byteOffset = 0;
+        }
+        else {
+          prevName = struct.fieldOrder[firstAnon_i-1];
+          const prevOffset = obj['byteOffset<' + prevName + '>'];
+          const prevLength = obj['byteLength<' + prevName + '>'];
+          if (prevOffset && prevLength && typeof prevOffset.value === 'number' && typeof prevLength.value === 'number') {
+            byteOffset = prevOffset.value + prevLength.value;
+          }
+        }
+        let anonLength = 0;
+        for (let j = firstAnon_i; j < i; j++) {
+          const anon = struct.fieldOrder[j];
+          const byteLength = anon.fixedByteLength;
+          if (isNaN(byteLength)) {
+            anonLength = -1;
+            break;
+          }
+          anonLength += byteLength;
+        }
+        if (byteOffset !== -1 && anonLength !== -1) {
+          byteOffset += anonLength;
+          obj['byteOffset<'+name+'>'] = {value:byteOffset};
+        }
+        else {
+          obj['byteOffset<'+name+'>'] = {
+            get: function() {
+              if (byteOffset === -1) {
+                byteOffset = this['byteOffset<'+prevName+'>'];
+              }
+              for (let j = firstAnon_i; j < i; j++) {
+                const anon = struct.fieldOrder[j];
+                const anonLength = anon.fixedByteLength;
+                if (!isNaN(anonLength)) {
+                  byteOffset += anonLength;
+                }
+                else {
+                  byteOffset += anon.getDataView(this.buffer, byteOffset).byteLength;
+                }
+              }
+              Object.defineProperty(this, 'byteOffset<'+name+'>', {value:byteOffset});
+              return byteOffset;
+            },
+            configurable: true,
+          };
+        }
+      }
+      else {
+        if (i === 0) {
+          byteOffset = 0;
+          obj['byteOffset<'+name+'>'] = {value:0};
+        }
+        else {
+          prevName = struct.fieldOrder[i-1];
+          const prevOffset = obj['byteOffset<'+prevName+'>'];
+          const prevLength = obj['byteLength<'+prevName+'>'];
+          if (prevOffset && prevLength && typeof prevOffset.value === 'number' && typeof prevLength.value === 'number') {
+            byteOffset = prevOffset.value + prevLength.value;
+            obj['byteOffset<'+name+'>'] = {value:byteOffset};
+          }
+          else {
+            byteOffset = -1;
+            obj['byteOffset<'+name+'>'] = {
+              get: function() {
+                let byteOffset = this['byteOffset<'+prevName+'>'] + this['byteLength<'+prevName+'>'];
+                Object.defineProperty(this, 'byteOffset<'+name+'>', {value:byteOffset});
+                return byteOffset;
+              },
+              configurable: true,
+            };
+          }
+        }
+      }
+      const byteLength = this.fixedByteLength;
+      if (isNaN(byteLength)) {
+        const fn = this.getDataView.bind(this);
+        obj['byteLength<' + name + '>'] = {
+          get: function() {
+            if (byteOffset === -1) {
+              byteOffset = this['byteOffset<' + name + '>'];
+            }
+            byteLength = fn(this.buffer, byteOffset).length;
+            Object.defineProperty(this, 'byteLength<'+name+'>', {value:byteLength});
+            return byteLength;
+          },
+        };
+      }
+      else {
+        obj['byteLength<'+name+'>'] = {value:byteLength};
+      }
+      const getter = this.readValue.bind(this);
+      const setter = this.writeValue.bind(this);
+      obj[name] = {
+        get: function() {
+          return getter(this.dv, this['byteOffset<'+name+'>']);
+        },
+        set: function(value) {
+          setter(this.dv, this['byteOffset<'+name+'>'], value);
+        },
+      };
+    },
   };
   
-  function StructFactory() {
+  fieldDefs.u8 = Object.assign(new StructFieldDef('u8'), {
+    fixedByteLength: 1,
+    readValue: function(dv, o) {
+      return dv.getUint8(o);
+    },
+    writeValue: function(dv, o, v) {
+      dv.setUint8(o, v);
+    },
+  });
+  
+  function StructDef(name) {
+    this.name = name;
     this.fieldDefs = Object.create(this.fieldDefs);
     this.fieldOrder = [];
     this.namedFields = Object.create(null);
   }
-  StructFactory.prototype = {
+  StructDef.prototype = Object.create(StructFieldDef);
+  Object.assign(StructDef.prototype, {
     fieldDefs: fieldDefs,
     endian: undefined,
     fieldOrder: undefined,
@@ -47,11 +204,11 @@ define(function() {
       }
       return this;
     },
-  };
+  });
   
   return {
+    Def: StructDef,
     FieldDef: StructFieldDef,
-    Factory: StructFactory,
     fieldDefs: fieldDefs,
   };
 
