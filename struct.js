e9f68c3e61002606516c0c25ffa30b272c4b4d87
intro-swf <33427068+intro-swf@users.noreply.github.com>
2018-06-29 10:25:23 +0100

    Update struct.js

--- struct.js
+++ struct.js
@@ -4,14 +4,53 @@ define(function() {
   
   const fieldDefs = Object.create(null);
   
+  function StructFieldDef(name) {
+    this.name = name;
+  }
+  StructFieldDef.prototype = {
+    name: undefined,
+  };
+  
   function StructFactory() {
     this.fieldDefs = Object.create(this.fieldDefs);
+    this.fieldOrder = [];
+    this.namedFields = Object.create(null);
   }
   StructFactory.prototype = {
     fieldDefs: fieldDefs,
+    endian: undefined,
+    fieldOrder: undefined,
+    namedFields: undefined,
+    packed: true,
+    padAlignBytes: 1,
+    
+    field: function(TDef, name) {
+      if (typeof TDef === 'string') {
+        if (!(TDef in this.fieldDefs)) {
+          throw new Error('unknown field def: ' + TDef);
+        }
+        TDef = this.fieldDefs[TDef];
+      }
+      else if (!(TDef instanceof StructFieldDef)) {
+        throw new TypeError('not a valid field def: ' + TDef);
+      }
+      const field = TDef;
+      if (name) {
+        if (name in this.fieldsByName) {
+          throw new Error('duplicate field name: ' + name);
+        }
+        this.fieldOrder.push(name);
+        this.namedFields[name] = field;
+      }
+      else {
+        this.fieldOrder.push(field);
+      }
+      return this;
+    },
   };
   
   return {
+    FieldDef: StructFieldDef,
     Factory: StructFactory,
     fieldDefs: fieldDefs,
   };
